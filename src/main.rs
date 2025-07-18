60d16027c5b24a496f0b3add9c8e1f9d3e3a11b5
Domenic Wolf <DomenicWolf@Protonmail.com>
2025-07-18 02:51:29 -0400

    Testing basic paddle

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use sdl2::rect::Rect;
 
 
 pub fn main() {
@@ -25,10 +26,12 @@ pub fn main() {
 
     let mut i = 0;
 
+    let mut test = Rect::new(100,100,100,200);
+
     'running: loop {
         i = (i + 1) % 255;
 
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.set_draw_color(Color::RGB(i, 0, 255 - i));
         canvas.clear();
 
 
@@ -39,9 +42,19 @@ pub fn main() {
                     break 'running
                 },
 
+                Event::KeyDown {keycode: Some(Keycode::W), ..} => {
+                    test.y = test.y - 10;
+                },
+
+                Event::KeyDown {keycode: Some(Keycode::S), ..} => {
+                    test.y = test.y + 10;
+                },
+
                 _ => {}
             }
         }
+        canvas.set_draw_color(Color::RGB(i,i,i));
+        canvas.fill_rect(test).unwrap();
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
